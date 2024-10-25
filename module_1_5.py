028eb6f464cb8e9b2a45ce31365252f3fa4b1043
Naitren <horrorkristi@gmail.com>
2024-10-25 20:54:41 +0700

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,6 +1,6 @@
 immutable_var=(1, True, 'string')
 print (immutable_var)
-#immutable_var[0]=200 - нельзя изменить, потому что элементы кортежа не меняются.'
+#immutable_var[0]=200 - нельзя изменить, потому что элементы кортежа не меняются.
 mutable_list = [1, True, 'string']
 print(mutable_list)
 mutable_list[1]=False
